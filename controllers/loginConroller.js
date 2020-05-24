c3361f7fa44b38f489282341f2a6e8e3805b7c6a
waleedj99 <waleedj1699@gmail.com>
2020-05-24 11:58:09 +0530

    bug fixes

--- controllers/loginConroller.js
+++ controllers/loginConroller.js
@@ -2,13 +2,12 @@ var bodyParser = require('body-parser');
 
 var urlencodeParser = bodyParser.urlencoded({extended:true});
 
-var hour = [00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
-
-var minu = [00,05,10,15,20,25,30,35,40,45,50,55] 
-
 module.exports = (app, data,MongoClient,url,selectedShop) => {
     app.use(bodyParser.urlencoded({ extended: false }));
     
+    app.get("/",(req,res)=>{
+        res.redirect("/select")
+    })
     app.get("/select",(req,res)=>{
         res.render("selector")
     })
--- index.js
+++ index.js
@@ -7,23 +7,6 @@ var MongoClient = require('mongodb').MongoClient
 var url ="mongodb+srv://waleed:wal40250@cluster0-rlbi7.mongodb.net/test?retryWrites=true&w=majority"//'mongodb://localhost:27017/shoplist'
 var app = express();
 
-/* var data = [{
-    'opening-hour': '4',
-    'opening-minu': '15',
-    'closing-hour': '9',
-    'closing-minu': '50',
-    'slot-duration': '10',
-    'bookings-per-slot': '5'
-  },
-  {
-    'opening-hour': '4',
-    'opening-minu': '15',
-    'closing-hour': '9',
-    'closing-minu': '50',
-    'slot-duration': '10',
-    'bookings-per-slot': '5'
-  } ]; */
-
 var data = [];
   
 var selectedShop=0
@@ -40,5 +23,7 @@ buyerController(app,data,MongoClient,url)
 
 loginController(app,data,MongoClient,url,selectedShop)
 
-app.listen(3000);
-console.log('[Listening] at port 3000')
+var port = process.env.PORT || 3000
+app.listen(port, function() {
+    console.log("To view your app, open this link in your browser: http://localhost:" + port);
+});
